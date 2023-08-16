0f4bcd1b8987e97fc1f71090798e1d2e5c5498d8
lilYummy228 <142146352+lilYummy228@users.noreply.github.com>
2023-08-16 13:09:09 +0500

    начальный commit

--- ex10/Program.cs
+++ ex10/Program.cs
@@ -11,6 +11,23 @@ namespace ex10
         static void Main(string[] args)
         {
             Random rand = new Random();
+            int number = rand.Next(0, 100);
+            Console.WriteLine($"Рандомное число: {number}");
+            int sumOfNumbers = 0;
+
+            for (int i = 1; i <= number; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    sumOfNumbers += i;
+                    Console.WriteLine($"Число кратное 3 или 5: {i}");
+                }
+                else
+                {
+                    continue;
+                }
+            }
+                Console.WriteLine($"Сумма чисел равна: {sumOfNumbers}");
         }
     }
 }
