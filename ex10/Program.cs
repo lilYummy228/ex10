54eb321b772b3b983b45bcc518cc589f422e6839
lilYummy228 <142146352+lilYummy228@users.noreply.github.com>
2023-08-16 12:31:17 +0500

    Добавьте файлы проекта.

---
+++ ex10/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+        }
+    }
+}
